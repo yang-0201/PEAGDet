b287d3a9dfcce3c15c196e3d64df430eec153c93
UltralyticsAssistant <web@ultralytics.com>
2026-02-23 04:22:05 +0000

    Auto-format by https://ultralytics.com/actions

--- save_txt.py
+++ save_txt.py
@@ -1,4 +1,5 @@
 import os
+
 path = r"E:\tianchi\Dual-MHAF-yolov12\runs\res152\labels"
 result_path = "./result.txt"
 image_path = r"E:\datasets\images\testB"
@@ -8,19 +9,17 @@ for filename in os.listdir(image_path):
         name_without_ext = os.path.splitext(filename)[0]
         filenames.append(name_without_ext)
 
-i =0
-with open(result_path, 'w', encoding='utf-8') as all_file:
-    all_file.write("8414735 15"+'\n')
+i = 0
+with open(result_path, "w", encoding="utf-8") as all_file:
+    all_file.write("8414735 15" + "\n")
     for file in os.listdir(path):
         name = file.split(".")[0]
-        while  filenames[i] != name:
-            all_file.write(filenames[i] + '.jpg' + '\n')
+        while filenames[i] != name:
+            all_file.write(filenames[i] + ".jpg" + "\n")
             i += 1
         i += 1
 
-
-
-        with open(os.path.join(path, file), 'r', encoding='utf-8') as txt_file:
+        with open(os.path.join(path, file), encoding="utf-8") as txt_file:
             this_result = ""
             lines = txt_file.readlines()
             for line in lines:
@@ -39,4 +38,4 @@ with open(result_path, 'w', encoding='utf-8') as all_file:
                     update_line = f"{box[0]} {box[1]} {box[2]} {box[3]} {conf} {class_number} "
 
                 this_result += update_line
-            all_file.write(name + '.jpg ' + this_result + '\n')
+            all_file.write(name + ".jpg " + this_result + "\n")
